802dcea3dd26ebf1da4f1e3830606cc39e6a05ff
foremtehan <foremtehan@gmail.com>
2024-01-07 17:02:36 +0330

    wip

--- src/Exceptions/TelegramResponseException.php
+++ src/Exceptions/TelegramResponseException.php
@@ -81,7 +81,7 @@ class TelegramResponseException extends TelegramSDKException
             default => static::class
         };
 
-        throw new $exception($response, new TelegramOtherException($message, $code));
+        throw new $exception($response);
     }
 
     /**
