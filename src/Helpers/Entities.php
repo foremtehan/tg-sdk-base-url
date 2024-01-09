8b6615bbde1350ddacf8ee7be71cf7d345b231a3
foremtehan <foremtehan@gmail.com>
2024-01-09 17:46:01 +0330

    wip

--- src/Helpers/Entities.php
+++ src/Helpers/Entities.php
@@ -79,21 +79,16 @@ class Entities
     {
         $syntax = $this->syntax();
 
-        $this->entities = array_reverse($this->entities);
-        foreach ($this->entities as $entity) {
+        foreach (array_reverse($this->entities) as $entity) {
             $value = mb_substr($this->text, $entity['offset'], $entity['length']);
             $type = $entity['type'];
-            if (isset($syntax[$type])) {
-                if ($type === 'text_link') {
-                    $replacement = sprintf($syntax[$type][$this->mode], $value, $entity['url']);
-                } else {
-                    $replacement = sprintf(
-                        $syntax[$type][$this->mode],
-                        ($type === 'text_mention') ? $entity['user']['username'] : $value
-                    );
-                }
-                $this->text = substr_replace($this->text, $replacement, $entity['offset'], $entity['length']);
-            }
+            $replacement = match ($type) {
+                'text_link' => sprintf($syntax[$type][$this->mode], $value, $entity['url']),
+                'text_mention' => sprintf($syntax[$type][$this->mode], $entity['user']['username']),
+                default => sprintf($syntax[$type][$this->mode], $value),
+            };
+
+            $this->text = substr_replace($this->text, $replacement, $entity['offset'], $entity['length']);
         }
 
         return $this->text;
@@ -110,12 +105,16 @@ class Entities
         // 'url', 'bot_command', 'hashtag', 'cashtag', 'email', 'phone_number', 'mention'
 
         return [
-            'bold'         => ['*%s*', '<strong>%s</strong>'],
-            'italic'       => ['_%s_', '<i>%s</i>'],
-            'code'         => ['`%s`', '<code>%s</code>'],
-            'pre'          => ["```\n%s```", '<pre>%s</pre>'],
+            'bold' => ['*%s*', '<strong>%s</strong>'],
+            'italic' => ['_%s_', '<i>%s</i>'],
+            'code' => ['`%s`', '<code>%s</code>'],
+            'pre' => ["```\n%s```", '<pre>%s</pre>'],
+            'spoiler' => ['||%s||', '<tg-spoiler>%s</tg-spoiler>'],
             'text_mention' => ['[%1$s](tg://user?id=%1$s)', '<a href="tg://user?id=%1$s">%1$s</a>'],
-            'text_link'    => ['[%s](%s)', '<a href="%2$s">%1$s</a>'],
+            'text_link' => ['[%s](%s)', '<a href="%2$s">%1$s</a>'],
+            'underline' => ['_%s_', '<u>%s</u>'],
+            'strikethrough' => ['~%s~', '<s>%s</s>'],
+            'block_quote'    => ['>%s', '<blockquote>%s</blockquote>'],
         ];
     }
 }
