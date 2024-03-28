22bcccae585e09313961005a24adbab295aaa911
foremtehan <foremtehan@gmail.com>
2024-03-28 08:20:47 +0430

    wip

--- src/Exceptions/TelegramResponseException.php
+++ src/Exceptions/TelegramResponseException.php
@@ -86,6 +86,7 @@ class TelegramResponseException extends TelegramSDKException
             $has('no write access') => NoWriteAccessException::class,
             $has('kicked from the supergroup') || $has('kicked from the group') => BotKickedFromGroupException::class,
             $has('kicked from the channel') => BotKickedFromChannelException::class,
+            $has('QUOTE_TEXT_INVALID') => QuoteInvalidException::class,
             $message == 'Unauthorized' || $has('SESSION_REVOKED') => UnauthorizedException::class,
             default => static::class
         };
