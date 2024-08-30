5bbe375b047bfd6c701c1ba39f2755438b0e8b18
foremtehan <foremtehan@gmail.com>
2024-08-30 17:39:03 +0430

    wip

--- src/Exceptions/TooManyRequestException.php
+++ src/Exceptions/TooManyRequestException.php
@@ -6,5 +6,8 @@ use Telegram\Bot\Exceptions\TelegramResponseException;
 
 class TooManyRequestException extends TelegramResponseException
 {
-
+    public function getTtl(): int
+    {
+        return $this->getResponseData()['parameters']['retry_after'] + 2;
+    }
 }
