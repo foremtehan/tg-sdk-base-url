eda1a61d9a98ac195289fde479e39f00cfcaaa24
foremtehan <foremtehan@gmail.com>
2024-11-13 08:20:06 +0330

    wip

--- src/Exceptions/TelegramResponseException.php
+++ src/Exceptions/TelegramResponseException.php
@@ -87,6 +87,7 @@ class TelegramResponseException extends TelegramSDKException
             $has('not a member of') => BotNotMemberOfChatException::class,
             $has('QUOTE_TEXT_INVALID') => QuoteInvalidException::class,
             $has('initiate') => BotCantInitiateConversationException::class,
+            $has('thread not found') => ThreadNotFoundException::class,
             $has('CHANNEL_FORUM_MISSING') || $has('not a forum') => ForumMissingException::class,
             $has('not enough rights') || $has('member list is inaccessible') || $has('rights in the') => NotEnoughRightsException::class,
             $message == 'Unauthorized' || $has('SESSION_REVOKED') || $has('USER_DEACTIVATED') => UnauthorizedException::class,
