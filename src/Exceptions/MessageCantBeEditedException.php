b61928fdadc2286b3296ca9d8088edab2fc92351
foremtehan <foremtehan@gmail.com>
2025-11-01 23:16:28 +0330

    wip

---
+++ src/Exceptions/MessageCantBeEditedException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Telegram\Bot\Exceptions;
+
+class MessageCantBeEditedException extends TelegramResponseException
+{
+
+}
--- src/Exceptions/TelegramResponseException.php
+++ src/Exceptions/TelegramResponseException.php
@@ -9,21 +9,16 @@ use Telegram\Bot\TelegramResponse;
  */
 class TelegramResponseException extends TelegramSDKException
 {
-    /** @var TelegramResponse The response that threw the exception. */
-    protected $response;
-
     /** @var array Decoded response. */
-    protected $responseData;
+    private array $responseData = [];
 
     /**
      * Creates a TelegramResponseException.
-     *
      * @param TelegramResponse $response The response that threw the exception.
-     * @param TelegramSDKException $previousException The more detailed exception.
+     * @param TelegramSDKException|null $previousException The more detailed exception.
      */
-    public function __construct(TelegramResponse $response, TelegramSDKException $previousException = null)
+    public function __construct(private TelegramResponse $response, ?TelegramSDKException $previousException = null)
     {
-        $this->response = $response;
         $this->responseData = $response->getDecodedBody();
 
         $errorMessage = $this->get('description', 'Unknown error from API Response.');
@@ -34,23 +29,17 @@ class TelegramResponseException extends TelegramSDKException
 
     /**
      * Checks isset and returns that or a default value.
-     *
      * @param string $key
-     * @param mixed $default
-     *
      * @return mixed
      */
-    protected function get($key, $default = null)
+    public function get($key, mixed $default = null)
     {
         return $this->responseData[$key] ?? $default;
     }
 
     /**
      * A factory for creating the appropriate exception based on the response from Telegram.
-     *
      * @param TelegramResponse $response The response that threw the exception.
-     *
-     * @return TelegramResponseException
      */
     public static function create(TelegramResponse $response)
     {
@@ -68,7 +57,7 @@ class TelegramResponseException extends TelegramSDKException
             $has('message to edit not found') => MessageToEditNotFoundException::class,
             $has('blocked by the user') || $has('USER_IS_BLOCKED') => BotBlockedByUserException::class,
             $has('user is deactivated') => UserDeactivatedException::class,
-            $has('message is too long') || $has('caption is too long') || $has('_TOO_LONG') => TextTooLongException::class,
+            $has('message is too long') || $has('text is too long') || $has('caption is too long') || $has('_TOO_LONG') => TextTooLongException::class,
             $has('Too Many Requests') => TooManyRequestException::class,
             $has('user not found') => UserNotFoundException::class,
             $has('chat not found') || $has('PEER_ID_INVALID') => ChatNotFoundException::class,
@@ -80,7 +69,7 @@ class TelegramResponseException extends TelegramSDKException
             $has('message to react not found') => MessageToReactNotFoundException::class,
             $has("can't be forwarded") => MessageCantBeForwardedException::class,
             $has("can't be copied") => MessageCantBeCopiedException::class,
-            $has("VOICE_MESSAGES_FORBIDDEN") => VoiceMessageForbiddenException::class,
+            $has("VOICE_MESSAGES_FORBIDDEN") || $has('video messages') => VoiceMessageForbiddenException::class,
             $has('no write access') => NoWriteAccessException::class,
             $has('kicked from the supergroup') || $has('kicked from the group') => BotKickedFromGroupException::class,
             $has('kicked from the channel') => BotKickedFromChannelException::class,
@@ -91,6 +80,8 @@ class TelegramResponseException extends TelegramSDKException
             $has('PARTICIPANT_ID_INVALID') => ParticipantInvalidException::class,
             $has('PEER_ID_INVALID') => PeerInvalidException::class,
             $has('member not found') => MemberNotFoundException::class,
+            $has("can't parse entities") => CantParseEntitiesException::class,
+            $has("can't be edited") => MessageCantBeEditedException::class,
             $has('CHANNEL_FORUM_MISSING') || $has('not a forum') => ForumMissingException::class,
             $has('not enough rights') || $has('member list is inaccessible') || $has('rights in the') => NotEnoughRightsException::class,
             $message == 'Unauthorized' || $has('SESSION_REVOKED') || $has('USER_DEACTIVATED') => UnauthorizedException::class,
